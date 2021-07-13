5e66da7f2665469bd74f84de88963c5d6a0cd4bd
areph <40balmung@gmail.com>
2021-07-13 22:09:04 +0900

    add sumOfArray test

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,5 +1,19 @@
-import {sumOfArray} from '../functions'
+import { sumOfArray } from '../functions'
 
-test('test', () => {
-  expect(sumOfArray([1, 2])).toBe(3);
+describe('sumOfArrayのテスト', () => {
+  test('1つしか値がない場合はその値を返す', () => {
+    expect(sumOfArray([100])).toBe(100);
+  })
+  test('2つの値がある場合はその合計値を返す', () => {
+    expect(sumOfArray([100, 200])).toBe(300);
+  })
+  test('3つの値がある場合はその合計値を返す', () => {
+    expect(sumOfArray([100, 200, 500])).toBe(800);
+  })
+  test('負数があっても合計値を返す', () => {
+    expect(sumOfArray([100, -200, 500])).toBe(400);
+  })
+  test('浮動小数点があっても合計値を返す', () => {
+    expect(sumOfArray([0.1, 0.2])).toBeCloseTo(0.3);
+  })
 })
