b42e36d1ebe62bb7c87b8f7459926b9ae16aa67b
areph <40balmung@gmail.com>
2021-07-14 21:18:59 +0900

    add save method mock when throw

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -34,7 +34,7 @@ describe('asyncSumOfArrayのテスト', () => {
 describe('asyncSumOfArraySometimesZeroのテスト', () => {
   beforeEach(() => {
     // Clear all instances and calls to constructor and all methods:
-    // mocked(DatabaseMock).mockClear();
+    mocked(DatabaseMock).mockClear();
   });
 
   test('エラーが発生しなければ引数に渡した合計値を返す', async () => {
@@ -44,5 +44,19 @@ describe('asyncSumOfArraySometimesZeroのテスト', () => {
       await expect(asyncSumOfArraySometimesZero([100])).resolves.toBe(100);
     }
     expect(DatabaseMock).toHaveBeenCalledTimes(100);
-  })
-})
+  });
+
+  test('エラーが発生した場合は0を返す', async () => {
+    // NOTE: Mockでエラーをthrowさせる
+    mocked(DatabaseMock).mockImplementation(() => {
+      return {
+        save: () => {
+          throw new Error('throw-mock');
+        },
+      };
+    });
+    expect(DatabaseMock).not.toHaveBeenCalled();
+    await expect(asyncSumOfArraySometimesZero([100])).resolves.toBe(0);
+    expect(DatabaseMock).toHaveBeenCalledTimes(1);
+  });
+});
