5b91d8fb0c97ce4f2b976c676cd7904fbd23366b
areph <40balmung@gmail.com>
2021-11-25 10:49:25 +0900

    fix lint.

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,36 +1,36 @@
-import { mocked  } from 'ts-jest/utils';
-import { sumOfArray, asyncSumOfArray, asyncSumOfArraySometimesZero, getFirstNameThrowIfLong } from '../functions'
-import { DatabaseMock } from "../util/index";
-import { NameApiService } from "../nameApiService";
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { mocked } from 'ts-jest/utils';
+import { sumOfArray, asyncSumOfArray, asyncSumOfArraySometimesZero, getFirstNameThrowIfLong } from '../functions';
+import { DatabaseMock } from '../util/index';
+import { NameApiService } from '../nameApiService';
 jest.mock('../util/index');
-// jest.mock('../nameApiService');
 
 describe('sumOfArrayのテスト', () => {
   test('1つしか値がない場合はその値を返す', () => {
     expect(sumOfArray([100])).toBe(100);
-  })
+  });
   test('2つの値がある場合はその合計値を返す', () => {
     expect(sumOfArray([100, 200])).toBe(300);
-  })
+  });
   test('3つの値がある場合はその合計値を返す', () => {
     expect(sumOfArray([100, 200, 500])).toBe(800);
-  })
+  });
   test('負数があっても合計値を返す', () => {
     expect(sumOfArray([100, -200, 500])).toBe(400);
-  })
+  });
   test('浮動小数点があっても合計値を返す', () => {
     expect(sumOfArray([0.1, 0.2])).toBeCloseTo(0.3);
-  })
+  });
   test('空の配列を渡した場合は例外を返す', () => {
     expect(() => sumOfArray([])).toThrow();
-  })
-})
+  });
+});
 
 describe('asyncSumOfArrayのテスト', () => {
   test('1つしか値がない場合はその値を返す', async () => {
     await expect(asyncSumOfArray([100])).resolves.toBe(100);
-  })
-})
+  });
+});
 
 describe('asyncSumOfArraySometimesZeroのテスト', () => {
   beforeEach(() => {
@@ -64,11 +64,11 @@ describe('asyncSumOfArraySometimesZeroのテスト', () => {
 
 describe('getFirstNameThrowIfLongのテスト', () => {
   test('指定した引数の数字が文字列長より小さければその文字列を返す', async () => {
-    const spyNameApiService = jest.spyOn(NameApiService.prototype, 'getFirstName').mockReturnValue(Promise.resolve('mockmock'));
-    await expect(getFirstNameThrowIfLong(10)).resolves.toBe("mockmock");
+    jest.spyOn(NameApiService.prototype, 'getFirstName').mockReturnValue(Promise.resolve('mockmock'));
+    await expect(getFirstNameThrowIfLong(10)).resolves.toBe('mockmock');
   });
   test('指定した引数の数字が文字列長より大きければ例外を返す', async () => {
-    const spyNameApiService = jest.spyOn(NameApiService.prototype, 'getFirstName').mockReturnValue(Promise.resolve('mockmock'));
-    await expect(getFirstNameThrowIfLong(1)).rejects.toThrow(new Error("first_name too long"));
+    jest.spyOn(NameApiService.prototype, 'getFirstName').mockReturnValue(Promise.resolve('mockmock'));
+    await expect(getFirstNameThrowIfLong(1)).rejects.toThrow(new Error('first_name too long'));
   });
 });
