76b2e1164bb2c867fa92683a15838dfcedd242d2
areph <40balmung@gmail.com>
2021-11-26 19:16:55 +0900

    add test function1

--- jestSample/__tests__/my_functions.test.ts
+++ jestSample/__tests__/my_functions.test.ts
@@ -1,9 +1,22 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { isPostalCode } from '../my_functions';
+import { isZipCode } from '../my_functions';
 jest.mock('../util/index');
 
-describe('isPostalCodeのテスト', () => {
-  test('空文字はfalsy', () => {
-    expect(isPostalCode('')).toBeFalsy();
+describe('isZipCodeのテスト', () => {
+  test('空文字はfalsy', async () => {
+    const result = await isZipCode('');
+    expect(result).toBeFalsy();
+  });
+  test('正しい郵便番号フォーマット以外はfalsy', async () => {
+    const result = await isZipCode('1234567');
+    expect(result).toBeFalsy();
+  });
+  test('存在しない郵便番号はfalsy', async () => {
+    const result = await isZipCode('123-4567');
+    expect(result).toBeFalsy();
+  });
+  test('存在する郵便番号はtrue', async () => {
+    const result = await isZipCode('222-0037');
+    expect(result).toBeTruthy();
   });
 });
--- jestSample/my_functions.ts
+++ jestSample/my_functions.ts
@@ -1,3 +1,14 @@
-export const isPostalCode = (postalCode: string): boolean => {
-  return false;
+import axios from 'axios';
+
+export const isZipCode = async (zipCode: string): Promise<boolean> => {
+  // NOTE: 外部のAPIをコールするため事前に入力形式が正しいかチェックする
+  // NOTE: 存在しない郵便コードが入力された場合はfalseを返却
+  if (!/\d{3}-\d{4}/.test(zipCode)) {
+    return false;
+  }
+  const { data } = await axios.get(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipCode}`);
+
+  const results = data.results;
+
+  return results != null && results.length != 0;
 };
